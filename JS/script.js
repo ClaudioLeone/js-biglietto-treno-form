9bbded74a5efdc19c989db14e9392b73684ebddd
ClaudioLeone <claleone92@gmail.com>
2023-03-09 20:46:36 +0100

    Working on logic

--- JS/script.js
+++ JS/script.js
@@ -9,4 +9,42 @@
         • limite decimali: 2 [xx.yy(OK) ; xx.yyzz...(KO)]
 */
 
-const totalKm = document.
+// const userKm = document.getElementById("user-km");
+// const userAge = document.getElementById("user-age");
+// const pricePerKm = 0.21;
+
+// LOGICA
+const userKm = document.getElementById("user-km").value;
+const userAge = document.getElementById("user-age").value;
+const generateBtn = document.getElementById("generate-btn");
+const pricePerKm = 0.21;
+
+let kmRate = userKm * 0.21;
+console.log(kmRate);
+
+generateBtn.addEventListener("click", function() {
+    const userKm = document.getElementById("user-km").value;
+    document.getElementById("user-km-stamp").innerHTML = userKm;
+    const userAge = document.getElementById("user-age");
+    document.getElementById("user-age-stamp").value = userAge.options[userAge.selectedIndex].text;
+});
+
+// OUTPUT
+if (userAge < 18){
+    kmRate = (kmRate - (20 / 100 * kmRate)).toFixed(2);
+    document.getElementById("user-km").innerHTML = userKm;
+    
+    document.getElementById("user-price").innerHTML = kmRate;
+} 
+else if (userAge >= 65){
+    kmRate = (kmRate - (40 / 100 * kmRate)).toFixed(2);
+    document.getElementById("user-km").innerHTML = userKm;
+
+    document.getElementById("user-price").innerHTML = kmRate;
+}
+else{
+    kmRate.toFixed(2);
+    document.getElementById("user-km").innerHTML = userKm;
+
+    document.getElementById("user-price").innerHTML = kmRate;
+}
