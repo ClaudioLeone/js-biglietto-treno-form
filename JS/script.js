352cf3982d4f4a63a910de94ab52d72c8dc45315
ClaudioLeone <claleone92@gmail.com>
2023-03-09 17:18:57 +0100

    Implemented basic form

---
+++ JS/script.js
@@ -0,0 +1,12 @@
+/*
+    Il programma dovrà:
+    - chiedere all'utente il numero di chilometri da percorrere;
+    - età del passeggero;
+    - calcolare il prezzo totale del viaggio:
+        • definito in base ai km (0.21 € al km)
+        • sconto del 20% per i minorenni
+        • sconto del 40% per gli over 65
+        • limite decimali: 2 [xx.yy(OK) ; xx.yyzz...(KO)]
+*/
+
+const totalKm = document.
